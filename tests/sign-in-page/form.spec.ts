343c1007edd6755be065e6b84df764272951960d
user123Diviya <2212036@nec.edu.in>
2026-04-22 20:27:42 +0530

    Create form.spec.ts

---
+++ tests/sign-in-page/form.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('form test', async ({ page }) => {
+  await page.goto('https://www.netflix.com/login');
+  await expect(page).toHaveTitle(/Netflix/);
+});
