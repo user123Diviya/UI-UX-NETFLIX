6fd8c3527678f1b3b5f6411cc3fa6b4db6dbee57
user123Diviya <2212036@nec.edu.in>
2026-04-22 19:31:08 +0530

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
+  }
+});
